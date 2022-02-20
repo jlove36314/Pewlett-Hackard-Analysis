6a5f5158cc1912d706f751ecfeff22418ba8d3a1
Jlove <j.love36314@gmail.com>
2022-02-20 11:31:52 -0500

    m

--- Queries/Employee_Database_challenge.sql.sql
+++ Queries/Employee_Database_challenge.sql.sql
@@ -174,3 +174,20 @@ INTO retiring_titles
 FROM unique_titles AS ut
 GROUP BY ut.title
 ORDER BY Count(ut.title) DESC;
+
+SELECT DISTINCT ON (emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON(e.emp_no = de.emp_no)
+INNER JOIN title AS ti
+ON(e.emp_no = ti.emp_no)
+WHERE de.to_date = '9999-01-01' 
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no
